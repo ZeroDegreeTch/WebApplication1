c19a468950d93a19173415ac3585eba432edad8b
高海峰 <feng22744543@outlook.com>
2019-12-27 11:52:07 +0800

    实现年级的新增,但是返回值存在着一些小问题

--- WebApplication1/Controllers/StudentController.cs
+++ WebApplication1/Controllers/StudentController.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-
+using WebApplication1.Models;
+using System.Data.Entity;
 namespace WebApplication1.Controllers
 {
     public class StudentController : ApiController
     {
 
+        public IHttpActionResult Post([FromBody] Grades g) 
+        {
+            EFContext ef = new EFContext();
 
+            ef.Entry<Grades>(g).State = EntityState.Added;
+            ef.SaveChanges();
+
+            return Ok();
+            
+        }
 
     }
 }
